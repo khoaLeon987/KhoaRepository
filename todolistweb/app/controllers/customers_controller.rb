324a22772f029726df97bc7c278c0d1de8c6a263
Tran DangKhoa <khoatran@rubify.com>
2012-02-13 16:27:58 +0700

    create customer action

---
+++ todolistweb/app/controllers/customers_controller.rb
@@ -0,0 +1,19 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+  def new
+    @customer = Customer.new
+  end
+  def create
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+       redirect_to(:action => 'index')
+    else
+       render('new')
+    end     
+  end    
+  def show
+    @customer = Customer.find(params[:id])
+  end  
+end
