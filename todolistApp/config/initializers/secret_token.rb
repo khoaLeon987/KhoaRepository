b84ecd4e3bdbf12f4fb3e3723e60255a3ed0e3a6
Tran DangKhoa <khoatran@rubify.com>
2012-02-13 10:44:06 +0700

    deleted project

--- todolistApp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TodolistApp::Application.config.secret_token = '0da0d905ff857a9462db457218d39d2a69f44354516d8fd6663c630ae62e5e20584aaf42af76db0cb726d62983822033f4266ca03a8be7d1b593ff1d2fa43b6e'
