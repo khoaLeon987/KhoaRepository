d9d852e984fac9ad85ecfec61bbb3767c37018be
Tran DangKhoa <khoatran@rubify.com>
2012-02-13 15:48:33 +0700

    create customer controller

--- todolistweb/app/models/customer.rb
+++ todolistweb/app/models/customer.rb
@@ -1,2 +1,14 @@
 class Customer < ActiveRecord::Base
+  
+   #has_many :todo_lists
+
+   validates :login_name , :uniqueness => true, :presence => true
+   validates :password , :presence => true , :confirmation => true
+   validates :email ,:uniqueness => true , :presence => true, :format => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ , :confirmation => true
+
+   attr_accessor :is_changing_password , :old_email, :is_changing_email , :new_pass, :old_pass, :new_pass_confirm
+
+   #validate :validate_during_changing_password , :validate_during_changing_email
+  
+  
 end
--- todolistweb/db/migrate/20120213035450_create_customers.rb
+++ todolistweb/db/migrate/20120213035450_create_customers.rb
@@ -1,7 +1,11 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-
+        t.string "login_name" , :limit => 50
+        t.string "full_name"  , :limit => 50
+        t.string "email"     , :default => "" , :null => false , :limit => 150
+        t.string "password"  , :default =>"" , :null => false , :limit => 50
+        t.timestamps
       t.timestamps
     end
   end
