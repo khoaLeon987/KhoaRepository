008fb81e2864df79ebb03886ee6cd8089ebfdb00
Tran DangKhoa <khoatran@rubify.com>
2012-02-13 10:49:00 +0700

    step 0: adding new project

---
+++ todolistweb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolistweb::Application.config.secret_token = '8ba0090b3231e85459d6d32dcaefdf3a7b5bf022bb0d484590d706b0c9d584f6eabad133a651d4c4a20ebb6f303708a79167ca3bd833cb8bb23aebbb79f289e9'
