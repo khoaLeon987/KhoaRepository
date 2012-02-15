3dda71fbde4dc4f8d3b20d177c0791460cfdf579
Tran DangKhoa <khoatran@rubify.com>
2012-02-15 09:23:27 +0700

    testing module

--- todolistweb/app/controllers/admin/admin_customer_controller.rb
+++ todolistweb/app/controllers/admin/admin_customer_controller.rb
@@ -1,4 +1,12 @@
-class AdminCustomerController < ApplicationController
+class Admin::AdminCustomerController < ApplicationController
   
+  http_basic_authenticate_with :name => 'admin' , :password => 'genoa'
+  
+  def index
+    @customers = Customer.all
+  end
+  def show
+    @customer = session[:customer]
+  end
   
 end
